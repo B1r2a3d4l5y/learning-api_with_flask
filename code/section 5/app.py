21b4133ca3865c63c9fbc481d9fe9053c38c72e4
B1r2a3d4l5y <82877822+B1r2a3d4l5y@users.noreply.github.com>
2021-08-20 10:18:11 +0200

    Update app.py

--- code/section 5/app.py
+++ code/section 5/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required, current_identity
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 
 from security import authenticate, identity
 from user import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
 
@@ -13,59 +14,13 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-items = []
 
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-        type = float,
-        required = True,
-        help="This field can not be blank!"
-    )
-
-    @jwt_required()
-    def get(self, name):
-        return {'item': next(filter(lambda x: x['name'] == name, items), None)}
-
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
-            return {'message': "An item with name '{}' already exists.".format(name)}
-
-        data = Item.parser.parse_args()
-
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item
-
-    @jwt_required()
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted'}
-
-    @jwt_required()
-    def put(self, name):
-        data = Item.parser.parse_args()
-        
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
 
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-api.add_resource(UserRegister,'/register')
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    app.run(debug=True)  
-
+    app.run(debug=True)
+        
